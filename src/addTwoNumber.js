8941af329bef90164caa643de8e954dfada345e3
Kenny Ho <kenny850917@hotmail.com>
2021-06-12 02:57:26 -0700

    split two functions

--- src/addTwoNumber.js
+++ src/addTwoNumber.js
@@ -1,11 +1,3 @@
-"use strict";
-
-module.exports.generateRandomNumber = (event) => {
-  const randomNumber = parseInt(Math.random() * 100);
-  console.log("The random generated integer is: ", randomNumber);
-  return randomNumber;
-};
-
 module.exports.addTwoNumber = (event) => {
   const firstNum = parseInt(Math.random() * 10);
   const secondNum = parseInt(Math.random() * 10);
